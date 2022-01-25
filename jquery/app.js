1d320ae7c4d581a1cad3ef8ec829012a67680821
gustavo-sobarzo <gustavo.a.sobarzo@gmail.com>
2022-01-25 00:28:19 -0300

    Update16

--- jquery/app.js
+++ jquery/app.js
@@ -8,7 +8,7 @@ const cargarApp = () => {
 
 let crearClienteHTML = (cliente) => {
   let formatoClienteHtml = `<tr><td>${cliente.nombre}</td><td>${cliente.apellido}</td><td>${cliente.email}</td>
-  <td><button class="elemento_eliminar--btn" onclick="eliminarIngreso(${cliente.id})"><ion-icon name="close-circle-outline"></ion-icon></button></td></tr>`;
+  <td><button class="elemento_eliminar--btn" onclick="eliminarCliente(${cliente.id})"><ion-icon name="close-circle-outline"></ion-icon></button></td></tr>`;
   return formatoClienteHtml;
 }
 
@@ -18,5 +18,29 @@ let crearListaCliente = () => {
     clienteHTML += crearClienteHTML(cliente)
   }
   return document.getElementById('tablita').innerHTML = clienteHTML;
+}
+
+const eliminarCliente = (id) => {
+  let clienteEleiminar = clientes.findIndex(cliente => cliente.id === id);
+  clientes.splice(clienteEleiminar, 1);
+  crearListaCliente();
+}
+
+let agregarCliente = () => {
+  let formulario = document.forms['form'];
+  let nombre = formulario['nombre'];
+  let apellido = formulario['apellido'];
+  let email = formulario['email'];
+
+  if (nombre !== '' && apellido !== '' && email !== '') {
+    let comprobarEmail = document.getElementById('email')
+
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+    clientes.push(new Cliente(nombre.value, apellido.value, email.value))
+    crearListaCliente();
+  } else {
+    alert('hay campos vacios!')
+  }
 }
 
